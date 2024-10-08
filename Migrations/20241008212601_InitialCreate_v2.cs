38aee8e18183ffb03e90581b7b643040becac8dd
sangnv <sangnv19093@gmail.com>
2024-10-09 04:30:20 +0700

    Update code

---
+++ Migrations/20241008212601_InitialCreate_v2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MFFMS.API.Migrations
+{
+    public partial class InitialCreate_v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TinhTrang",
+                table: "DanhSachHoaDonDichVu",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NgayLap",
+                table: "DanhSachDonNhapHang",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "TinhTrang",
+                table: "DanhSachDonNhapHang",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TinhTrang",
+                table: "DanhSachHoaDonDichVu");
+
+            migrationBuilder.DropColumn(
+                name: "NgayLap",
+                table: "DanhSachDonNhapHang");
+
+            migrationBuilder.DropColumn(
+                name: "TinhTrang",
+                table: "DanhSachDonNhapHang");
+        }
+    }
+}
